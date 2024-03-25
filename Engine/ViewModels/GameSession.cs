dc2761869c1991f70c6dbc61f9d2b45cd4da85f9
Embedded-Tinkerer <70577913+Embedded-Tinkerer@users.noreply.github.com>
2024-03-25 13:07:01 -0400

    updateUI

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,14 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    internal class GameSession
+    {
+        Player CurrentPlayer { get; set; };
+    }
+}
