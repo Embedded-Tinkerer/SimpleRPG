ac651088443cfcedd5c187cc1861a87de0029a9d
Embedded-Tinkerer <70577913+Embedded-Tinkerer@users.noreply.github.com>
2024-03-26 08:55:25 -0400

    gamesession update v2

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,21 +1,23 @@
-﻿using Engine.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Engine.Models;
 namespace Engine.ViewModels
 {
-    internal class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; };
+        public Player CurrentPlayer { get; set; }
         public GameSession()
         {
-            CurrentPlayer = new Models.Player();
-            CurrentPlayer.Name = "Brian";
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Scott";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
             CurrentPlayer.Gold = 1000000;
-
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
         }
     }
 }
